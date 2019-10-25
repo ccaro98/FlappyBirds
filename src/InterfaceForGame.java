bedbb61585bb33def1a8d34413b18173dcb1ca63
Cristian Caro <ccaro98>
2019-10-24 21:14:54 -0500

    Starting the interface for the game

--- src/InterfaceForGame.java
+++ src/InterfaceForGame.java
@@ -1,3 +1,13 @@
-public class InterfaceForGame {
-    
+import javax.swing.*;
+
+public class InterfaceForGame extends Rectangles {
+
+    public final int height = 800, width = 800;
+
+    public InterfaceForGame()
+    {
+        JFrame window = new JFrame("Game");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
+    }
 }
