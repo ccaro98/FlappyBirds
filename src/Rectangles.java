1c1942f9def9fdc71ba4266117ff1c03e4132fbc
Daniel Chang <changdaniel025@gmail.com>
2019-10-24 23:10:38 -0400

    Created Rectangle class

--- src/Rectangles.java
+++ src/Rectangles.java
@@ -2,7 +2,7 @@ import java.awt.Rectangle;
 
 public class Rectangles
 {
-  Rectangle rectangle_obstacle = new Rectangle(10,10,50,100);
+ // Rectangle rectangle_obstacle = new Rectangle(10,10,50,100);
 
 
 }
